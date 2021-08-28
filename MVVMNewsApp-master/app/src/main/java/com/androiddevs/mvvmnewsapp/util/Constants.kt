1ea17bf3965ab2a954e3409102dbac49322de4ee
Priyanka <priyanka.chowdhury2020@vitstudent.ac.in>
2021-08-28 16:04:46 +0530

    till retrofit:

---
+++ MVVMNewsApp-master/app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val  API_KEY = "3c0bfc6b106b49cd90fc6efaedda509d"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
